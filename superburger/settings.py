b4c33c79b54e510b13f224f15cc34d2abf8d6c08
3mawy <m.3mawyy@gmail.com>
2022-01-23 02:39:32 +0200

    fix

--- superburger/settings.py
+++ superburger/settings.py
@@ -180,4 +180,4 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 django_heroku.settings(locals())
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
