1a6074df8fa13019d9daf3b212a5e67409f056fb
3mawy <m.3mawyy@gmail.com>
2023-08-15 08:12:23 +0300

    add cart serializers

--- app/api/serializers/CartItemSerializer.py
+++ app/api/serializers/CartItemSerializer.py
@@ -3,7 +3,8 @@ from api.models import CartItem
 
 
 class CartItemSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = CartItem
         depth = 1
-        fields = '__all__'
+        fields = ('id', 'quantity', 'item_price', 'total_price', 'comments', 'extras_price', 'size', 'offer', 'item', 'extras' )
--- app/api/serializers/CartSerializer.py
+++ app/api/serializers/CartSerializer.py
@@ -1,10 +1,15 @@
 from rest_framework import serializers
 from api.models import  Cart
 
+from api.serializers.CartItemSerializer import CartItemSerializer
+from api.serializers import OrderItemSerializer
+
 
 class CartSerializer(serializers.ModelSerializer):
+    cart_items = CartItemSerializer(many=True, required=False)
+
     class Meta:
         model = Cart
         depth = 1
-        fields = '__all__'
+        fields = ('id', 'total_price', 'cart_items')
 
